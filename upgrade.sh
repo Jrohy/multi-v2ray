64287327e573327b93ab1f5a89589e3181c2b1b9
Jrohy <1101400284jackie@gmail.com>
2017-10-26 16:51:18 -0400

    更新升级脚本

--- upgrade.sh
+++ upgrade.sh
@@ -16,5 +16,13 @@ chmod +x /usr/local/bin/v2ray
 #更新Vray主程序
 bash <(curl -L -s https://install.direct/go.sh)
 
+#初始化环境
+rm -rf /etc/v2ray/config.json
+mv /usr/local/v2ray.fun/json_template/server.json /etc/v2ray/config.json
+UUID=$(cat /proc/sys/kernel/random/uuid)
+sed -i "s/cc4f8d5b-967b-4557-a4b6-bde92965bc27/${UUID}/g" /etc/v2ray/config.json
+python /usr/local/v2ray.fun/genclient.py
+python /usr/local/v2ray.fun/openport.py
+
 clear
 echo "脚本已更新！"

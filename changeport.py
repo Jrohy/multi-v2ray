e15803dc1ae9181e5bbceab8dd6bf53bdc4c3277
Jrohy <euvkzx@gmail.com>
2018-03-06 22:02:49 +0800

    增加修改alterId

--- changeport.py
+++ changeport.py
@@ -2,29 +2,13 @@
 # -*- coding: utf-8 -*-
 import readjson
 import writejson
-
-#判断是否为数字的函数
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
- 
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
- 
-    return False
+import v2rayutil
 
 #主要程序部分
 print ("当前主端口为：%s") % str(readjson.ConfPort)
 print ("请输入新端口：")
 newport=raw_input()
-if (is_number(newport)):
+if (v2rayutil.is_number(newport)):
     writejson.WritePort(newport)
 else:
     print ("输入错误，请检查是否为数字")

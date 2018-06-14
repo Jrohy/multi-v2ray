57609e1d74628f3ae4fd09eb32f99775a39c45d5
Jrohy <euvkzx@gmail.com>
2018-06-15 01:45:16 +0800

    初始化fix bug

--- read_json.py
+++ read_json.py
@@ -1,31 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import json
-from base_util import v2ray_util
-
-with open('/etc/v2ray/config.json', 'r') as json_file:
-    config = json.load(json_file)
-
-#读取配置文件大框架
-conf_inbound=config[u"inbound"]
-conf_outbound=config[u"outbound"]
-conf_inboundDetour=config[u"inboundDetour"]
-conf_outboundDetour=config[u"outboundDetour"]
-conf_dns=config[u"dns"]
-conf_routing=config[u"routing"]
-
-if "detour" in conf_inbound[u"settings"]:
-    dyp_AId=""
-    for detour_list in conf_inboundDetour:
-        if "tag" in detour_list and detour_list[u"tag"] == "detour":
-            dyp_AId = detour_list[u"settings"][u"default"][u"alterId"]
-            break
-    conf_Dyp="开启,alterId为 %s" % dyp_AId
-else:
-    conf_Dyp="关闭"
-
-#获取本机IP地址
-conf_ip = v2ray_util.get_ip()
 
 def read_sin_user(part_json, multi_user_conf, index_dict):
     conf_path=""
@@ -84,15 +59,42 @@ def read_sin_user(part_json, multi_user_conf, index_dict):
         sinUserConf['indexDict']=copyIndexDict
         multi_user_conf.append(sinUserConf)
 
-multiUserConf=[]
-#indexDict存储节点在json的索引，和节点代表的组别
-#读取inbound节点
-index_dict={"inboundOrDetour":0, "detourIndex":0, "clientIndex":0, "group":"A"}
-read_sin_user(conf_inbound, multiUserConf, index_dict)
+if __name__ == '__main__':
+    
+    with open('/etc/v2ray/config.json', 'r') as json_file:
+        config = json.load(json_file)
 
-#读取inboundDetour节点
-index_dict['inboundOrDetour']=1
-if conf_inboundDetour != None:
-    for index,detour_list in enumerate(conf_inboundDetour):
-        index_dict['detourIndex']=index
-        read_sin_user(detour_list, multiUserConf, index_dict)
+    #读取配置文件大框架
+    conf_inbound=config[u"inbound"]
+    conf_outbound=config[u"outbound"]
+    conf_inboundDetour=config[u"inboundDetour"]
+    conf_outboundDetour=config[u"outboundDetour"]
+    conf_dns=config[u"dns"]
+    conf_routing=config[u"routing"]
+
+    if "detour" in conf_inbound[u"settings"]:
+        dyp_AId=""
+        for detour_list in conf_inboundDetour:
+            if "tag" in detour_list and detour_list[u"tag"] == "detour":
+                dyp_AId = detour_list[u"settings"][u"default"][u"alterId"]
+                break
+        conf_Dyp="开启,alterId为 %s" % dyp_AId
+    else:
+        conf_Dyp="关闭"
+
+    #获取本机IP地址
+    from base_util import v2ray_util
+    conf_ip = v2ray_util.get_ip()
+
+    multiUserConf=[]
+    #indexDict存储节点在json的索引，和节点代表的组别
+    #读取inbound节点
+    index_dict={"inboundOrDetour":0, "detourIndex":0, "clientIndex":0, "group":"A"}
+    read_sin_user(conf_inbound, multiUserConf, index_dict)
+
+    #读取inboundDetour节点
+    index_dict['inboundOrDetour']=1
+    if conf_inboundDetour != None:
+        for index,detour_list in enumerate(conf_inboundDetour):
+            index_dict['detourIndex']=index
+            read_sin_user(detour_list, multiUserConf, index_dict)

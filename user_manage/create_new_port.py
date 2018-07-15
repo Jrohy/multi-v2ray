79947d7eb7b327abbd5ef46fc197b937caeb121d
Jrohy <euvkzx@gmail.com>
2018-07-15 16:38:17 +0800

    create random port && clean v2ray log

--- user_manage/create_new_port.py
+++ user_manage/create_new_port.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import write_json
+import random
 from base_util import tool_box
 
-new_port=input("请输入新端口：")
+random_port = random.randint(1000, 65535)
+new_port=input("产生随机端口%d, 回车直接以该端口新建Group, 否则输入自定义端口: " % random_port)
+
+if not new_port:
+    new_port = random_port
+    
 if tool_box.is_number(new_port):
+    print("新端口为: %d" % new_port)
     write_json.create_new_port(new_port)
 else:
     print ("\n输入错误，请检查是否为数字")

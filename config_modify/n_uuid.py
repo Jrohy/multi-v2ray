8393238227d10d436ba2c56b14ecb57b060e8493
Jrohy <1101400284jackie@gmail.com>
2018-07-27 23:56:42 +0800

    fix mtproto

--- config_modify/n_uuid.py
+++ config_modify/n_uuid.py
@@ -16,18 +16,21 @@ if length > 1:
     choice=input("请输入要改UUID的节点序号数字:")
     if not tool_box.is_number(choice):
         print("输入错误，请检查是否为数字")
-        exit
+        exit()
     choice = int(choice)
 
 if length == 1 or (choice > 0 and choice <= len(mul_user_conf)):
-    print ("当前节点UUID为：%s" % mul_user_conf[choice - 1]['id'])
-    if_gen_uuid=input("是否要随机生成一个新的UUID (y/n)：")
-    if if_gen_uuid=="y":
-        new_uuid = uuid.uuid1()
-        print("新的UUID为：%s" % new_uuid)
-        write_json.write_uuid(new_uuid, mul_user_conf[choice - 1]['indexDict'])
-        print("UUID修改成功！")
+    if mul_user_conf[choice - 1]["protocol"] == "vmess":
+        print ("当前节点UUID为：%s" % mul_user_conf[choice - 1]['id'])
+        if_gen_uuid=input("是否要随机生成一个新的UUID (y/n)：")
+        if if_gen_uuid=="y":
+            new_uuid = uuid.uuid1()
+            print("新的UUID为：%s" % new_uuid)
+            write_json.write_uuid(new_uuid, mul_user_conf[choice - 1]['indexDict'])
+            print("UUID修改成功！")
+        else:
+            print("已取消生成新的UUID,未执行任何操作")
     else:
-        print("已取消生成新的UUID,未执行任何操作")
+        print("只有vmess协议才能修改alterId!")
 else:
     print ("输入错误，请检查是否符合范围中")

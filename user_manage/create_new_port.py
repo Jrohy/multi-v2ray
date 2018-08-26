6758089f9b7258fbe644dd6297b3d58278105d63
Jrohy <euvkzx@gmail.com>
2018-08-26 23:29:04 +0800

    优化显示

--- user_manage/create_new_port.py
+++ user_manage/create_new_port.py
@@ -32,7 +32,7 @@ else:
     salt_protocol=["dtls", "wechat", "utp", "srtp"]
     random.shuffle(salt_protocol)
     protocol=salt_protocol[0]
-    print("随机一种 (srtp | wechat-video | utp | dtls) header伪装, 当前生成 %s" % protocol)
+    print("随机一种 (srtp | wechat-video | utp | dtls) header伪装, 当前生成 %s \n" % protocol)
 
 random_port = random.randint(1000, 65535)
 new_port=input("产生随机端口%d, 回车直接以该端口新建Group, 否则输入自定义端口: " % random_port)
@@ -41,7 +41,7 @@ if not new_port:
     new_port = random_port
 
 if tool_box.is_number(new_port):
-    print("新端口为: %d \n" % int(new_port))
+    print("\n新端口为: %d \n" % int(new_port))
     write_json.create_new_port(new_port, protocol, **info)
 else:
     print ("\n输入错误，请检查是否为数字")

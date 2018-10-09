ae7220d047891e21198d7c5453cea93ad6fad7a7
Jrohy <euvkzx@gmail.com>
2018-10-09 21:42:54 +0800

    fix dyp

--- config_modify/dyn_port.py
+++ config_modify/dyn_port.py
@@ -12,11 +12,11 @@ else:
     print('当前组的动态端口状态：{}'.format(group.dyp))
     gw = GroupWriter(group.tag, group.index)
     
-    choice = input("是否开启动态端口(y/n)").lower()
+    choice = input("是否开启动态端口(y/n): ").lower()
 
     if choice == 'y':
         newAlterId = input("请为动态端口设置alterID(默认32): ")
-        newAlterId = '32' if newAlterId == '' else ''
+        newAlterId = '32' if newAlterId == '' else newAlterId
         if (newAlterId.isdecimal()):
             gw.write_dyp(True, newAlterId)
             print("\n成功开启动态端口!")

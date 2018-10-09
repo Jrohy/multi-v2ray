56484d3c0774ddc3a0b2551ec784014a10c99a3e
Jrohy <1101400284@qq.com>
2018-10-09 20:34:53 +0800

    取消别名

--- selector.py
+++ selector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from loader import Loader
+from utils import color_str, Color
 
 class Selector:
     def __init__(self, action):
@@ -24,12 +25,12 @@ class ClientSelector(Selector):
         choice = input("请输入要{}的节点序号数字: ".format(self.action))
 
         if not choice.isnumeric():
-            print('输入错误，请检查是否为数字')
+            print(color_str(Color.RED, '输入错误，请检查是否为数字'))
             return
 
         choice = int(choice)
         if choice < 1 or choice > self.list_size:
-            print('输入错误，请检查是否符合范围中')
+            print(color_str(Color.RED, '输入错误，请检查是否符合范围中'))
         else:
             find = False
             for group in self.group_list:
@@ -54,7 +55,7 @@ class GroupSelector(Selector):
         print(self.profile)
         choice = input("请输入要{}的节点Group字母: ".format(self.action)).upper()
         if len(choice) != 1 or not choice.isalpha() or choice > self.group_list[-1].tag:
-            print('输入有误，请检查是否为字母且范围中')
+            print(color_str(Color.RED, '输入有误，请检查是否为字母且范围中'))
             self.group = None
         else:
             self.group = [x for x in self.group_list if x.tag == choice][0]

783c4b0fd07c0dfd181dda770345f6d85f600d49
Jrohy <euvkzx@gmail.com>
2018-11-18 18:53:42 +0800

    Fix: 寻找不到conf文件的bug

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import configparser
 
+CONF_FILE_PATH = '/usr/local/multi-v2ray/multi-v2ray.conf'
+
 # ENV = 'dev-path'
 ENV = 'prod-path'
 
@@ -10,7 +12,7 @@ class Config:
     def __init__(self):
         #读取配置文件
         self.config = configparser.ConfigParser()
-        self.config.read("multi-v2ray.conf")
+        self.config.read(CONF_FILE_PATH)
 
     def get_path(self, key):
         return self.config.get(ENV, key)

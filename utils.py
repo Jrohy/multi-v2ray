e71cea5f858635077f249b225493711b83f4ae2b
Jrohy <euvkzx@gmail.com>
2019-01-05 18:26:21 +0800

    Fix first install :bug:

--- utils.py
+++ utils.py
@@ -168,8 +168,8 @@ def calcul_iptables_traffic(port):
         upload_traffic = bytes_2_human_readable(int(traffic_list[0]), 2)
         download_traffic = bytes_2_human_readable(int(traffic_list[1]), 2)
         total_traffic = bytes_2_human_readable(int(traffic_list[2]), 2)
-        return "{0}:  upload:{1} download:{2} total:{3}".format(color_str(Color.GREEN, str(port)), 
-                color_str(Color.CYAN, upload_traffic), color_str(Color.CYAN, download_traffic), color_str(Color.CYAN, total_traffic))
+        return "{0}:  upload:{1} download:{2} total:{3}".format(ColorStr.green(str(port)), 
+                ColorStr.cyan(upload_traffic), ColorStr.cyan(download_traffic), ColorStr.cyan(total_traffic))
 
 def clean_iptables(port):
     clean_cmd = "iptables -D {0} {1}"

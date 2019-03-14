64c711065a7142463571f556cd0ee045adcb7d34
Jrohy <euvkzx@gmail.com>
2019-03-15 01:00:40 +0800

    调整脚本

--- multi-v2ray.sh
+++ multi-v2ray.sh
@@ -8,15 +8,10 @@ BEIJING_UPDATE_TIME=3
 #记录最开始运行脚本的路径
 BEGIN_PATH=$(pwd)
 
-#安装方式0: 全新安装, 1:保留配置更新 , 2:仅更新multi-v2ray源码
-INSTARLL_WAY=0
-
 #定义操作变量, 0为否, 1为是
 HELP=0
 
 REMOVE=0
-
-FORCE=0
 
 IS_LATEST=0
 
@@ -50,21 +45,10 @@ while [[ $# > 0 ]];do
         -h|--help)
         HELP=1
         ;;
-        -f|--force)
-        FORCE=1
-        ;;
         -v|--version)
         UPDATE_VERSION="$2"
-        echo -e "更新multi-v2ray到 $(colorEcho ${BLUE} $UPDATE_VERSION) 版本\n"
+        echo -e "update multi-v2ray to $(colorEcho ${BLUE} $UPDATE_VERSION) version\n"
         shift
-        ;;
-        -k|--keep)
-        INSTARLL_WAY=1
-        colorEcho ${BLUE} "当前以keep保留配置文件形式更新, 若失败请用全新安装\n"
-        ;;
-        -c|--code)
-        INSTARLL_WAY=2
-        colorEcho ${BLUE} "当前仅更新multi-v2ray源码\n"
         ;;
         *)
                 # unknown option
@@ -74,35 +58,11 @@ while [[ $# > 0 ]];do
 done
 #############################
 
-checkUpdate(){
-    LASTEST_VERSION=$(curl -H 'Cache-Control: no-cache' -s "https://api.github.com/repos/Jrohy/multi-v2ray/releases/latest" | grep 'tag_name' | cut -d\" -f4)
-
-    if [[ -e /usr/local/bin/v2ray ]];then
-        VERSION_TEMP_VALUE=$(cat /usr/local/bin/v2ray|grep SHELL_V2RAY|awk 'NR==1'|sed 's/\"//g')
-        if [[ ! -z $VERSION_TEMP_VALUE ]]; then
-            CURRENT_VERSION=${VERSION_TEMP_VALUE/*=}
-            if [[ ! -z $UPDATE_VERSION && $UPDATE_VERSION == $CURRENT_VERSION ]];then
-                echo -e "multi-v2ray当前版本: $(colorEcho $GREEN $CURRENT_VERSION), 已是指定版本!!!"
-                IS_LATEST=1
-                return
-            fi
-            if [[ -z $UPDATE_VERSION && $FORCE == 0 && $INSTARLL_WAY != 0 && $LASTEST_VERSION == $CURRENT_VERSION ]]; then
-                echo -e "multi-v2ray当前版本: $(colorEcho $GREEN $CURRENT_VERSION), 已是最新!!!"
-                IS_LATEST=1
-                return
-            fi
-        fi
-    fi
-
-    [[ -z $UPDATE_VERSION ]] && UPDATE_VERSION=$LASTEST_VERSION
-}
-
 help(){
-    echo "source multi-v2ray.sh [-h|--help] [-k|--keep] [-c|--code] [-f|--force] [--remove]"
+    echo "source multi-v2ray.sh [-h|--help] [-k|--keep] [-c|--code] [--remove]"
     echo "  -h, --help           Show help"
     echo "  -k, --keep           keep the v2ray config.json to update"
     echo "  -c, --code           only update multi-v2ray code"
-    echo "  -f, --force          force to update multi-v2ray lastest code"
     echo "  -v, --version        update multi-v2ray to special version"
     echo "      --remove         remove v2ray && multi-v2ray"
     echo "                       no params to new install"
@@ -228,94 +188,63 @@ planUpdate(){
 updateProject() {
     local DOMAIN=""
 
-    if [[ -e $APP_PATH/my_domain ]];then
-        DOMAIN=$(cat $APP_PATH/my_domain|awk 'NR==1')
-    elif [[ -e $APP_PATH/multi-v2ray.conf ]];then
+    if ! type pip >/dev/null 2>&1;then
+        colorEcho $RED "pip no install!"
+        exit 1
+    fi
+
+    if [[ -e /usr/local/multi-v2ray/multi-v2ray.conf ]];then
         TEMP_VALUE=$(cat $APP_PATH/multi-v2ray.conf|grep domain|awk 'NR==1')
         DOMAIN=${TEMP_VALUE/*=}
-    fi
-
-    cd /usr/local/
-    if [[ -e multi-v2ray && -e multi-v2ray/.git ]];then
-        cd multi-v2ray
-
-        COMMITTER_EMAIL=$(git log -1 --pretty=format:"%ae")
-        if [[ $COMMITTER_EMAIL =~ 'euvkzx' ]];then
-            git reset --hard HEAD && git clean -d -f
-            if [[ $FORCE == 1 ]]; then
-                git pull origin master
-            else
-                git fetch origin && git checkout $UPDATE_VERSION
-            fi
-        else
-            cd /usr/local/
-            rm -rf multi-v2ray
-            git clone https://github.com/Jrohy/multi-v2ray
-            cd multi-v2ray && git checkout $UPDATE_VERSION
+        if [[ ! -e /etc/v2ray_util/util.cfg ]];then
+            mkdir -p /etc/v2ray_util
+            curl https://raw.githubusercontent.com/Jrohy/multi-v2ray/dev-pip/v2ray_util/util_core/util.cfg > /etc/v2ray_util/util.cfg
+            [[ ! -z $DOMAIN ]] && sed -i "s/^domain.*/domain=${DOMAIN}/g" /etc/v2ray_util/util.cfg
         fi
-    else
-        git clone https://github.com/Jrohy/multi-v2ray
-        cd multi-v2ray && git checkout $UPDATE_VERSION
     fi
+    [[ -e /usr/local/multi-v2ray ]] && rm -rf /usr/local/multi-v2ray
 
-    [[ ! -z $DOMAIN ]] && sed -i "s/^domain.*/domain=${DOMAIN}/g" $APP_PATH/multi-v2ray.conf
+    [[ $UPDATE_VERSION ]] && pip3 install v2ray_util==$UPDATE_VERSION || pip3 install -U v2ray_util
+
+    rm -f /usr/local/bin/v2ray >/dev/null 2>&1
+    ln -s /usr/bin/v2ray-util /usr/local/bin/v2ray
 
     #更新v2ray bash_completion脚本
-    cp -f $APP_PATH/v2ray.bash /etc/bash_completion.d/
+    curl https://raw.githubusercontent.com/Jrohy/multi-v2ray/dev-pip/v2ray.bash > /etc/bash_completion.d/v2ray.bash
     [[ -z $(echo $SHELL|grep zsh) ]] && source /etc/bash_completion.d/v2ray.bash
     
     #安装/更新V2ray主程序
-    [[ ${INSTARLL_WAY} != 2 ]] && bash <(curl -L -s https://install.direct/go.sh)
+    bash <(curl -L -s https://install.direct/go.sh)
 }
 
 #时间同步
 timeSync() {
     if [[ ${INSTARLL_WAY} == 0 ]];then
         systemctl stop ntp &>/dev/null
-        echo -e "${Info} 正在进行时间同步 ${Font}"
+        echo -e "${Info} Time Synchronizing.. ${Font}"
         ntpdate time.nist.gov
         if [[ $? -eq 0 ]];then 
-            echo -e "${OK} 时间同步成功 ${Font}"
-            echo -e "${OK} 当前系统时间 `date -R`${Font}"
+            echo -e "${OK} Time Sync Success ${Font}"
+            echo -e "${OK} now: `date -R`${Font}"
             sleep 1
         else
-            echo -e "${Error} 时间同步失败，可以手动执行命令同步:${Font}${Yellow}ntpdate time.nist.gov${Font}"
+            echo -e "${Error} Time sync fail, please run command to sync:${Font}${Yellow}ntpdate time.nist.gov${Font}"
         fi
     fi
 }
 
 
 profileInit() {
-    rm -f /usr/local/bin/v2ray >/dev/null 2>&1
-    #配置V2ray初始环境
-    cp -f $APP_PATH/v2ray /usr/local/bin
-    chmod +x /usr/local/bin/v2ray
 
-    #加入multi-v2ray模块搜索路径
-    [[ -z $(grep multi-v2ray ~/$ENV_FILE) ]] && echo "export PYTHONPATH=$PYTHONPATH:$APP_PATH" >> ~/$ENV_FILE && source ~/$ENV_FILE
+    #清理v2ray模块环境变量
+    [[ $(grep v2ray ~/$ENV_FILE) ]] && sed -i '/v2ray/d' ~/$ENV_FILE && source ~/$ENV_FILE
 
     # 加入v2ray tab补全环境变量
     [[ -z $(echo $SHELL|grep zsh) && -z $(grep v2ray.bash ~/$ENV_FILE) ]] && echo "source /etc/bash_completion.d/v2ray.bash" >> ~/$ENV_FILE && source ~/$ENV_FILE
 
-    #解决Python3中文显示问题
-    [[ -z $(grep PYTHONIOENCODING=utf-8 ~/$ENV_FILE) ]] && echo "export PYTHONIOENCODING=utf-8" >> ~/$ENV_FILE && source ~/$ENV_FILE
-
     #全新安装的新配置
     if [[ ${INSTARLL_WAY} == 0 ]];then 
-        rm -rf /etc/v2ray/config.json
-        cp $APP_PATH/json_template/server.json /etc/v2ray/config.json
-
-        #产生随机uuid
-        UUID=$(cat /proc/sys/kernel/random/uuid)
-        sed -i "s/cc4f8d5b-967b-4557-a4b6-bde92965bc27/${UUID}/g" /etc/v2ray/config.json
-
-        #产生随机端口
-        D_PORT=$(shuf -i 1000-65535 -n 1)
-        sed -i "s/999999999/${D_PORT}/g" /etc/v2ray/config.json
-
-        #产生默认配置mkcp+随机3种伪装类型type
-        python3 -c "from config_modify import stream; stream.StreamModifier().random_kcp();"
-        python3 $APP_PATH/client.py
+        v2ray new
     else
         python3 $APP_PATH/converter.py
     fi
@@ -348,8 +277,6 @@ main() {
     [[ ${HELP} == 1 ]] && help && return
 
     [[ ${REMOVE} == 1 ]] && removeV2Ray && return
-
-    [[ ${FORCE} == 1 ]] && colorEcho ${BLUE} "当前为强制更新模式, 会更新到master最新代码\n"
 
     checkUpdate && [[ $IS_LATEST == 1 ]] && return
 

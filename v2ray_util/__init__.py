b0459e49734a138498bb4e7c609b987f62014ca8
Jrohy <euvkzx@gmail.com>
2019-03-19 23:47:32 +0800

    开始翻译之路..

--- v2ray_util/__init__.py
+++ v2ray_util/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '3.0.6'
+
+import gettext
+import pkg_resources
+
+gettext.translation('lang', pkg_resources.resource_filename('v2ray_util', 'locale_i18n'), languages=['zh_CH']).install()

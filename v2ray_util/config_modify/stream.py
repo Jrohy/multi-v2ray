f8260a138c80967fa740f4fc4728ee5bdc818c85
Jrohy <euvkzx@gmail.com>
2019-03-21 00:32:01 +0800

    完成大体翻译

--- v2ray_util/config_modify/stream.py
+++ v2ray_util/config_modify/stream.py
@@ -37,13 +37,13 @@ class StreamModifier:
         if index == 0 or (index >= 3 and index <= 9) or index == 11:
             pass
         elif index == 1 or index == 2:
-            host = input("please input fake domain: ")
+            host = input(_("please input fake domain: "))
             kw['host'] = host
         elif index == 10:
-            user = input("please input socks user: ")
-            password = input("please input socks password: ")
+            user = input(_("please input socks user: "))
+            password = input(_("please input socks password: "))
             if user == "" or password == "":
-                print("socks user or password is null")
+                print(_("socks user or password is null!!"))
                 exit(-1)
             kw = {'user': user, 'pass': password}
         elif index == 12:
@@ -53,26 +53,26 @@ class StreamModifier:
             key = ""
             security_list = ('none', "aes-128-gcm", "chacha20-poly1305")
             print("")
-            security = CommonSelector(security_list, "please select ss method: ").select()
+            security = CommonSelector(security_list, _("please select ss method: ")).select()
             if security != "none":
                 key = ''.join(random.sample(string.ascii_letters + string.digits, 8))
-                new_pass = input('random generate password {}, enter to use, or input customize password: '.format(key))
+                new_pass = input('{} {}, {}'.format(_("random generate password"), key, _("enter to use, or input customize password: ")))
                 if new_pass:
                     key = new_pass
                     
             print("")
-            header = CommonSelector(header_type_list(), "please select fake header: ").select()
+            header = CommonSelector(header_type_list(), _("please select fake header: ")).select()
             kw = {'security': security, 'key': key, 'header': header}
         sw.write(**kw)
 
     def random_kcp(self):
         kcp_list = ('mKCP + srtp', 'mKCP + utp', 'mKCP + wechat-video', 'mKCP + dtls')
         choice = random.randint(4, 7)
-        print("random generate (srtp | wechat-video | utp | dtls) fake header, new protocol: {} \n".format(ColorStr.green(kcp_list[choice - 4])))
+        print("{}: {} \n".format(_("random generate (srtp | wechat-video | utp | dtls) fake header, new protocol"), ColorStr.green(kcp_list[choice - 4])))
         self.select(choice)
 
 def modify():
-    gs = GroupSelector('modify protocol')
+    gs = GroupSelector(_('modify protocol'))
     group = gs.group
 
     if group == None:
@@ -80,7 +80,7 @@ def modify():
     else:
         sm = StreamModifier(group.tag, group.index)
 
-        print("group protocol: {}".format(group.node_list[0].stream()))
+        print("{}: {}".format(_("group protocol"), group.node_list[0].stream()))
         print ("")
         for index, stream_type in enumerate(sm.stream_type):
             print("{0}.{1}".format(index + 1, stream_type[1]))
@@ -88,13 +88,13 @@ def modify():
         choice = input()
 
         if not choice.isdecimal():
-            print("please input number!")
+            print(_("please input number!"))
         else:
             choice = int(choice)
             if choice > 0 and choice <= len(sm.stream_type):
                 if (sm.stream_type[choice - 1][1] == "MTProto" or sm.stream_type[choice - 1][1] == "Shadowsocks") and group.tls == 'tls':
-                    print("v2ray MTProto/Shadowsocks not support https, close tls success!")
+                    print(_("V2ray MTProto/Shadowsocks not support https, close tls success!"))
                 sm.select(choice - 1)
-                print("modify protocol success")
+                print(_("modify protocol success"))
             else:
-                print("input out of range!!")
+                print(_("input out of range!!"))

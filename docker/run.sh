5d8b4a9a230c645ec46b54499d229bf836297ac9
Jrohy <euvkzx@gmail.com>
2019-03-31 21:43:32 +0800

    fix bug

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ ! -e /etc/v2ray ]];then
-    mkdir -p /etc/v2ray
-    mkdir -p /var/log/v2ray/
+    mkdir /etc/v2ray
     v2ray clean >/dev/null 2>&1
     v2ray new >/dev/null 2>&1
 fi

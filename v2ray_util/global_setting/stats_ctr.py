81a3500bda2e15c829226ab1d6dde0288f172b01
Jrohy <euvkzx@gmail.com>
2019-11-05 01:07:00 +0800

    v2ray 流量统计优化

--- v2ray_util/global_setting/stats_ctr.py
+++ v2ray_util/global_setting/stats_ctr.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import subprocess
 
 from ..util_core.v2ray import V2ray
 from ..util_core.loader import Loader
@@ -14,36 +15,42 @@ class StatsFactory:
         self.downlink_value = 0
         self.uplink_value = 0
 
+    def __run_command(self, command):
+        value = 0
+        result = bytes.decode(subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.strip())
+        result_list = re.findall(r"\d+", result)
+        if result and len(result_list) == 1:
+            value = int(result_list[0])
+        return value
+
     def get_stats(self, meta_info, is_reset=False, is_group=False):
         is_reset = "true" if is_reset else "false"
 
-        stats_cmd = "cd /usr/bin/v2ray && ./v2ctl api --server=127.0.0.1:%s StatsService.GetStats 'name: \"%s>>>%s>>>traffic>>>%s\" reset: %s'"
+        stats_cmd = "cd /usr/bin/v2ray && ./v2ctl api --server=127.0.0.1:{} StatsService.GetStats 'name: \"{}>>>{}>>>traffic>>>{}\" reset: {}'"
         type_tag = ("inbound" if is_group else "user")
 
-        stats_real_cmd = stats_cmd % (str(self.door_port), type_tag, meta_info, "downlink", is_reset)
-        downlink_result = os.popen(stats_real_cmd).readlines()
-        if downlink_result and len(downlink_result) == 5:
-            re_result = re.findall(r"\d+", downlink_result[2])
-            if not re_result:
-                print(_("no data traffic now!"))
-                return
-            self.downlink_value = int(re_result[0])
+        stats_real_cmd = stats_cmd.format(str(self.door_port), type_tag, meta_info, "downlink", is_reset)
+        self.downlink_value = self.__run_command(stats_real_cmd)
 
-        stats_real_cmd = stats_cmd % (str(self.door_port), type_tag, meta_info, "uplink", is_reset)
-        uplink_result = os.popen(stats_real_cmd).readlines()
-        if uplink_result and len(uplink_result) == 5:
-            re_result = re.findall(r"\d+", uplink_result[2])
-            self.uplink_value = int(re_result[0])
+        stats_real_cmd = stats_cmd.format(str(self.door_port), type_tag, meta_info, "uplink", is_reset)
+        self.uplink_value = self.__run_command(stats_real_cmd)
 
-    def print_stats(self):
-        print('''
+    def print_stats(self, horizontal=False):
+        if horizontal:
+            print("downlink: {0} uplink: {1} total: {2}".format(
+                ColorStr.cyan(bytes_2_human_readable(self.downlink_value, 2)),
+                ColorStr.cyan(bytes_2_human_readable(self.uplink_value, 2)),
+                ColorStr.cyan(bytes_2_human_readable(self.downlink_value + self.uplink_value, 2)))
+            )
+        else:
+            print('''
 downlink: {0}  
 uplink: {1} 
 total: {2}
-        '''.format(ColorStr.cyan(bytes_2_human_readable(self.downlink_value, 2)),
-        ColorStr.cyan(bytes_2_human_readable(self.uplink_value, 2)),
-        ColorStr.cyan(bytes_2_human_readable(self.downlink_value + self.uplink_value, 2)))
-        )
+            '''.format(ColorStr.cyan(bytes_2_human_readable(self.downlink_value, 2)),
+            ColorStr.cyan(bytes_2_human_readable(self.uplink_value, 2)),
+            ColorStr.cyan(bytes_2_human_readable(self.downlink_value + self.uplink_value, 2)))
+            )
 
 def manage():
 
@@ -65,14 +72,22 @@ def manage():
         print("")
         print(_("2.close statistics"))
         print("")
-        print(_("3.check statistics result"))
+        print(_("3.check user statistics result"))
         print("")
-        print(_("4.reset statistics"))
+        print(_("4.check group statistics result"))
         print("")
-        print(_("tip: only have email node can statistics, restart v2ray will reset traffic statistics!!!"))
+        print(_("5.reset statistics"))
+        print("")
+        print(_("tip: restart v2ray will reset traffic statistics!!!"))
         print("")
 
         choice = input(_("please select: "))
+
+        if choice in ("3", "4", "5") and not profile.stats.status:
+            print(_("only open traffic statistics to operate"))
+            print("")
+            continue
+
         if choice == "1":
             if os.popen(FIND_V2RAY_CRONTAB_CMD).readlines():
                 rchoice = input(_("open traffic statistics will close schedule update v2ray, continue?(y/n): "))
@@ -95,17 +110,48 @@ def manage():
             print(_("close traffic statistics success!"))
             print("")
 
-        elif choice == "3" or choice == "4":
-            is_reset = (False if choice == "3" else True)
-            action_info = ("check" if choice == "3" else "reset")
-            if not profile.stats.status:
-                print("{} {}".format(_("only open traffic statistics can"), action_info))
+        elif choice == "3":
+            sf = StatsFactory(profile.stats.door_port)
+            print("")
+            for group in group_list:
+                tls = _("open") if group.tls == "tls" else _("close")
+                port_way = "-{}".format(group.end_port) if group.end_port else ""
+                for node in group.node_list:
+                    print('''
+Group: {group.tag}
+IP: {color_ip}
+Port: {group.port}{port_way}
+TLS: {tls}
+{node}             
+                    '''.format(group=group, color_ip=ColorStr.fuchsia(group.ip), node=node, tls=tls, port_way=port_way).strip())
+                    if node.user_info:
+                        sf.get_stats(node.user_info, False)
+                        sf.print_stats(horizontal=True)
+                    else:
+                        print(ColorStr.yellow(_("no effective email!!!")))
+                    print("")
+
+
+        elif choice == "4":
+            sf = StatsFactory(profile.stats.door_port)
+            print("")
+            for group in group_list:
+                tls = _("open") if group.tls == "tls" else _("close")
+                port_way = "-{}".format(group.end_port) if group.end_port else ""
+                print('''
+Group: {group.tag}
+IP: {color_ip}
+Port: {group.port}{port_way}
+TLS: {tls}
+                '''.format(group=group, color_ip=ColorStr.fuchsia(group.ip), tls=tls, port_way=port_way).strip())
+                sf.get_stats(group.tag, False, True)
+                sf.print_stats(horizontal=True)
                 print("")
-                continue
-            
+
+        elif choice == "5":
             if group_list[-1].node_list[-1].user_number > 1:
                 print(profile)
-                schoice = input("{} {}: ".format(_("please input number or group alphabet to"), action_info))
+                schoice = input(_("please input number or group alphabet to reset: "))
             else:
                 schoice = "A"
 
@@ -118,10 +164,10 @@ def manage():
                     for group in group_list:
                         if find:
                             break
-                        for index, node in enumerate(group.node_list):
+                        for node in group.node_list:
                             if node.user_number == schoice:
                                 if node.user_info:
-                                   sf.get_stats(node.user_info, is_reset)
+                                   sf.get_stats(node.user_info, True)
                                    sf.print_stats()
                                 else:
                                     print(_("no effective email!!!"))
@@ -129,7 +175,7 @@ def manage():
                                 find = True
                                 break
             elif schoice.isalpha() and schoice <= group_list[-1].tag:
-                sf.get_stats(schoice, is_reset, True)
+                sf.get_stats(schoice, True, True)
                 sf.print_stats()
             else:
                 print(_("input error, please input again"))

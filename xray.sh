8a6d14804a605691d2333378380700eeda01dc85
Radium-bit <Radiumbit@protonmail.com>
2022-04-14 17:09:21 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: Jrohy
-# github: https://github.com/Jrohy/multi-v2ray
+# github: https://github.com/Jrohy/multi-xray
 
 #定时任务北京执行时间(0~23)
 BEIJING_UPDATE_TIME=3
@@ -8,7 +8,7 @@ BEIJING_UPDATE_TIME=3
 #记录最开始运行脚本的路径
 BEGIN_PATH=$(pwd)
 
-#安装方式, 0为全新安装, 1为保留v2ray配置更新
+#安装方式, 0为全新安装, 1为保留xray配置更新
 INSTALL_WAY=0
 
 #定义操作变量, 0为否, 1为是
@@ -20,13 +20,13 @@ CHINESE=0
 
 BASE_SOURCE_PATH="https://multi.netlify.app"
 
-UTIL_PATH="/etc/v2ray_util/util.cfg"
+UTIL_PATH="/etc/xray_util/util.cfg"
 
-UTIL_CFG="$BASE_SOURCE_PATH/v2ray_util/util_core/util.cfg"
+UTIL_CFG="$BASE_SOURCE_PATH/xray_util/util_core/util.cfg"
 
-BASH_COMPLETION_SHELL="$BASE_SOURCE_PATH/v2ray"
+BASH_COMPLETION_SHELL="$BASE_SOURCE_PATH/xray"
 
-CLEAN_IPTABLES_SHELL="$BASE_SOURCE_PATH/v2ray_util/global_setting/clean_iptables.sh"
+CLEAN_IPTABLES_SHELL="$BASE_SOURCE_PATH/xray_util/global_setting/clean_iptables.sh"
 
 #Centos 临时取消别名
 [[ -f /etc/redhat-release && -z $(echo $SHELL|grep zsh) ]] && unalias -a
@@ -72,41 +72,41 @@ done
 #############################
 
 help(){
-    echo "bash v2ray.sh [-h|--help] [-k|--keep] [--remove]"
+    echo "bash xray.sh [-h|--help] [-k|--keep] [--remove]"
     echo "  -h, --help           Show help"
     echo "  -k, --keep           keep the config.json to update"
-    echo "      --remove         remove v2ray,xray && multi-v2ray"
+    echo "      --remove         remove xray,xray && multi-xray"
     echo "                       no params to new install"
     return 0
 }
 
-removeV2Ray() {
-    #卸载V2ray脚本
+removexray() {
+    #卸载xray脚本
     bash <(curl -L -s https://multi.netlify.app/go.sh) --remove >/dev/null 2>&1
-    rm -rf /etc/v2ray >/dev/null 2>&1
-    rm -rf /var/log/v2ray >/dev/null 2>&1
+    rm -rf /etc/xray >/dev/null 2>&1
+    rm -rf /var/log/xray >/dev/null 2>&1
 
     #卸载Xray脚本
     bash <(curl -L -s https://multi.netlify.app/go.sh) --remove -x >/dev/null 2>&1
     rm -rf /etc/xray >/dev/null 2>&1
     rm -rf /var/log/xray >/dev/null 2>&1
 
-    #清理v2ray相关iptable规则
+    #清理xray相关iptable规则
     bash <(curl -L -s $CLEAN_IPTABLES_SHELL)
 
-    #卸载multi-v2ray
-    pip uninstall v2ray_util -y
-    rm -rf /usr/share/bash-completion/completions/v2ray.bash >/dev/null 2>&1
-    rm -rf /usr/share/bash-completion/completions/v2ray >/dev/null 2>&1
+    #卸载multi-xray
+    pip uninstall xray_util -y
+    rm -rf /usr/share/bash-completion/completions/xray.bash >/dev/null 2>&1
     rm -rf /usr/share/bash-completion/completions/xray >/dev/null 2>&1
-    rm -rf /etc/bash_completion.d/v2ray.bash >/dev/null 2>&1
-    rm -rf /usr/local/bin/v2ray >/dev/null 2>&1
-    rm -rf /etc/v2ray_util >/dev/null 2>&1
+    rm -rf /usr/share/bash-completion/completions/xray >/dev/null 2>&1
+    rm -rf /etc/bash_completion.d/xray.bash >/dev/null 2>&1
+    rm -rf /usr/local/bin/xray >/dev/null 2>&1
+    rm -rf /etc/xray_util >/dev/null 2>&1
     rm -rf /etc/profile.d/iptables.sh >/dev/null 2>&1
     rm -rf /root/.iptables >/dev/null 2>&1
 
-    #删除v2ray定时更新任务
-    crontab -l|sed '/SHELL=/d;/v2ray/d'|sed '/SHELL=/d;/xray/d' > crontab.txt
+    #删除xray定时更新任务
+    crontab -l|sed '/SHELL=/d;/xray/d'|sed '/SHELL=/d;/xray/d' > crontab.txt
     crontab crontab.txt >/dev/null 2>&1
     rm -f crontab.txt >/dev/null 2>&1
 
@@ -116,8 +116,8 @@ removeV2Ray() {
         systemctl restart cron >/dev/null 2>&1
     fi
 
-    #删除multi-v2ray环境变量
-    sed -i '/v2ray/d' ~/$ENV_FILE
+    #删除multi-xray环境变量
+    sed -i '/xray/d' ~/$ENV_FILE
     sed -i '/xray/d' ~/$ENV_FILE
     source ~/$ENV_FILE
 
@@ -198,31 +198,31 @@ EOF
         $IPTABLE_WAY-save -c > /root/.iptables
     fi
 
-    pip install -U v2ray_util
+    pip install -U xray_util
 
     if [[ -e $UTIL_PATH ]];then
         [[ -z $(cat $UTIL_PATH|grep lang) ]] && echo "lang=en" >> $UTIL_PATH
     else
-        mkdir -p /etc/v2ray_util
+        mkdir -p /etc/xray_util
         curl $UTIL_CFG > $UTIL_PATH
     fi
 
     [[ $CHINESE == 1 ]] && sed -i "s/lang=en/lang=zh/g" $UTIL_PATH
 
-    rm -f /usr/local/bin/v2ray >/dev/null 2>&1
-    ln -s $(which v2ray-util) /usr/local/bin/v2ray
     rm -f /usr/local/bin/xray >/dev/null 2>&1
-    ln -s $(which v2ray-util) /usr/local/bin/xray
+    ln -s $(which xray-util) /usr/local/bin/xray
+    rm -f /usr/local/bin/xray >/dev/null 2>&1
+    ln -s $(which xray-util) /usr/local/bin/xray
 
-    #移除旧的v2ray bash_completion脚本
-    [[ -e /etc/bash_completion.d/v2ray.bash ]] && rm -f /etc/bash_completion.d/v2ray.bash
-    [[ -e /usr/share/bash-completion/completions/v2ray.bash ]] && rm -f /usr/share/bash-completion/completions/v2ray.bash
+    #移除旧的xray bash_completion脚本
+    [[ -e /etc/bash_completion.d/xray.bash ]] && rm -f /etc/bash_completion.d/xray.bash
+    [[ -e /usr/share/bash-completion/completions/xray.bash ]] && rm -f /usr/share/bash-completion/completions/xray.bash
 
-    #更新v2ray bash_completion脚本
-    curl $BASH_COMPLETION_SHELL > /usr/share/bash-completion/completions/v2ray
+    #更新xray bash_completion脚本
+    curl $BASH_COMPLETION_SHELL > /usr/share/bash-completion/completions/xray
     curl $BASH_COMPLETION_SHELL > /usr/share/bash-completion/completions/xray
     if [[ -z $(echo $SHELL|grep zsh) ]];then
-        source /usr/share/bash-completion/completions/v2ray
+        source /usr/share/bash-completion/completions/xray
         source /usr/share/bash-completion/completions/xray
     fi
     
@@ -249,14 +249,14 @@ timeSync() {
 
 profileInit() {
 
-    #清理v2ray模块环境变量
-    [[ $(grep v2ray ~/$ENV_FILE) ]] && sed -i '/v2ray/d' ~/$ENV_FILE && source ~/$ENV_FILE
+    #清理xray模块环境变量
+    [[ $(grep xray ~/$ENV_FILE) ]] && sed -i '/xray/d' ~/$ENV_FILE && source ~/$ENV_FILE
 
     #解决Python3中文显示问题
     [[ -z $(grep PYTHONIOENCODING=utf-8 ~/$ENV_FILE) ]] && echo "export PYTHONIOENCODING=utf-8" >> ~/$ENV_FILE && source ~/$ENV_FILE
 
     #全新安装的新配置
-    [[ ${INSTALL_WAY} == 0 ]] && v2ray new
+    [[ ${INSTALL_WAY} == 0 ]] && xray new
 
     echo ""
 }
@@ -266,14 +266,14 @@ installFinish() {
     cd ${BEGIN_PATH}
 
     [[ ${INSTALL_WAY} == 0 ]] && WAY="install" || WAY="update"
-    colorEcho  ${GREEN} "multi-v2ray ${WAY} success!\n"
+    colorEcho  ${GREEN} "multi-xray ${WAY} success!\n"
 
     if [[ ${INSTALL_WAY} == 0 ]]; then
         clear
 
-        v2ray info
+        xray info
 
-        echo -e "please input 'v2ray' command to manage v2ray\n"
+        echo -e "please input 'xray' command to manage xray\n"
     fi
 }
 
@@ -282,7 +282,7 @@ main() {
 
     [[ ${HELP} == 1 ]] && help && return
 
-    [[ ${REMOVE} == 1 ]] && removeV2Ray && return
+    [[ ${REMOVE} == 1 ]] && removexray && return
 
     [[ ${INSTALL_WAY} == 0 ]] && colorEcho ${BLUE} "new install\n"
 
